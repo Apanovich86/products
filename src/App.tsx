ddf15a2aae55849552c4e664e70930f9f7f0bef6
Apanovich86 <apanovich.anna.itec@gmail.com>
2024-05-13 10:56:56 +0200

    high_order_component_hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {FC, memo, useCallback, useState} from 'react';
 import './App.css';
 import usePrevious from './CustomHooks'
 
+const A:FC<any> = memo(({foobar}) => {
+    console.log('A render');
+    return <></>
+})
 const App = () => {
-    const usePreviousValue = usePrevious.usePrevious(100);
-    return (
-        <div>
-<div>{usePreviousValue}</div>
-        </div>
-    );
+   console.log('App render');
+   const [counter, setCounter] = useState(0);
+   const foobar = useCallback(() => {
+
+   }, [])
+    const [item1, setItem1] = useState('asd');
+    const [item2, setItem2] = useState('qwe');
+
+   return (
+       <div>
+           <A foobar={foobar} arr={[item1, item2]}/>
+           <button onClick={() => {
+               setCounter(counter+1);
+           }}>click</button>
+       </div>
+   )
 };
 
 export default App;
